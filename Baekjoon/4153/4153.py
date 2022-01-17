93407c645640dac9d9a6eeb27d58766a6c84f9cb
kimkh05 <nestjs01@naver.com>
2022-01-17 21:51:34 +0900

    2022.01.17 python & C 로 알고리즘 문제 풀어보기

---
+++ Baekjoon/4153/4153.py
@@ -0,0 +1,10 @@
+d = true
+while d == true:
+    a, b, c = map(int, input().split())
+    if a == 0 and b == 0 and c == 0 :
+        break
+    a *= a
+    b *= b
+    c*= c
+    if a + b == c or b + c == a or c + a == b : print("right")
+    else : print("wrong")
