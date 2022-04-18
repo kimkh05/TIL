f040d12cec1400958a2ad78b15b47cf02e2e1f08
kimkh05 <nestjs01@naver.com>
2022-04-18 23:37:51 +0900

    react thunk로 프로미스 다루기

--- learn-redux-middleware/src/App.js
+++ learn-redux-middleware/src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
-import CounterContainer from "./containers/counterContainer";
+import { Route, Routes } from "react-router-dom";
+import PostListPage from "./pages/PostListPage";
+import PostPage from "./pages/PostPage";
 
 function App() {
-  return <CounterContainer />;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={PostListPage} />
+        <Route path="/:id" element={PostPage} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
