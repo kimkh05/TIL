09696d29fcbe1035434ede632ec243c50e25a38e
kimkh05 <nestjs01@naver.com>
2022-04-18 23:38:07 +0900

    react thunk로 프로미스 다루기

---
+++ learn-redux-middleware/src/pages/postListPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import PostListContainer from "../containers/postContainer";
+
+function PostListPage() {
+  return <PostListContainer />;
+}
+
+export default PostListPage;
