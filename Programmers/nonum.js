587005da3d4aff84e8eee74d54d17e0218ae2e58
kimkh05 <nestjs01@naver.com>
2022-04-27 13:19:13 +0900

    programmers 문제 해결

---
+++ Programmers/nonum.js
@@ -0,0 +1,23 @@
+function solution(numbers) {
+  var answer = -1;
+  const LEN = numbers.length;
+  const numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+  const NUMLEN = numArr.length;
+  let sum = 0;
+  for (let i = 0; i < LEN; i++) {
+    for (let j = 0; j < NUMLEN; j++) {
+      numbers[i] === numArr[j] ? (numArr[j] = null) : null;
+      console.log("numArr["+ j + "] : " + numArr[i]);
+    }
+  }
+  for (let i = 0; i < NUMLEN; i++) {
+    if (numArr[i] !== null) {
+      sum += numArr[i];
+    }
+  }
+  answer = sum;
+  console.log("answer : " + answer);
+  return answer;
+}
+
+solution([1, 2, 3, 4, 6, 7, 8, 0]);
