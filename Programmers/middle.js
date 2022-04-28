765ce22c6bc11cc8187054911aeb2b7041dc32bd
kimkh05 <nestjs01@naver.com>
2022-04-28 09:35:54 +0900

    문제 해결

---
+++ Programmers/middle.js
@@ -0,0 +1,14 @@
+function solution(s) {
+  var answer = "";
+  const LEN = s.length;
+  let middle = LEN % 2 === 0 ? 2 : 1;
+  let a = Math.floor(LEN / 2);
+  middle === 2
+    ? (answer = s[LEN / 2 - 1] + s[LEN / 2])
+    : (answer = s[a]);
+  console.log(answer);
+  return answer;
+}
+
+solution("abcde");
+solution("qwer");
