e5589230fda1092acceea29fca1fe1983c30920f
kimkh05 <nestjs01@naver.com>
2022-05-31 16:56:00 +0900

    MODIFY : 기본 Js 테스트

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -1,30 +1,28 @@
 <script>
-	export let name;
+	let name = "홍길동";
+	let checkTrue = false;
+	
+	const click = () => {
+		console.log(checkTrue);
+		if(checkTrue){
+			name = "hwang";
+		} else{
+			name = "홍길동";
+		}
+		checkTrue = !checkTrue;
+	}
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<span>{name}</span>
+	<button on:click={click} >click me</button>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	body{
+		margin: 0;
+		padding: 0;
+		width: 100%;
+		height: 100%;
 	}
 </style>
