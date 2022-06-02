36e4d3958b4dfe24561713dd8d5b50569754d389
kimkh05 <nestjs01@naver.com>
2022-06-02 16:59:59 +0900

    NEXT 파일 구조 공부

--- next_practice/pages/_document.js
+++ next_practice/pages/_document.js
@@ -1,5 +1,5 @@
-import Document, { Head, Main, NextScript } from 'next/document';
-import React from 'react';
+import Document, { Head, Main, NextScript } from "next/document";
+import React from "react";
 
 export default class MyDocument extends Document {
   render() {
@@ -10,8 +10,7 @@ export default class MyDocument extends Document {
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
-          // 여기에 폰트 임포팅
-          // 여기에 공통 CSS e.g.reset-css/common.css
+          // 여기에 폰트 임포팅 // 여기에 공통 CSS e.g.reset-css/common.css
         </Head>
         <body>
           <div id="root">
