da0c94d10a728aee12b9508aaa046f2e83d56a71
kimkh05 <nestjs01@naver.com>
2022-06-07 19:31:10 +0900

    React 18버전 (useTransition , useDeferredValue) 사용

--- max/src/App.js
+++ max/src/App.js
@@ -1,8 +1,28 @@
-import TestModal from './components/testModal/testModal';
+import { useDeferredValue, useState, useTransition } from "react";
+import TestModal from "./components/testModal/testModal";
+
+let a = new Array(1000).fill(0);
 
 function App() {
+  const [name, setName] = useState("");
+  const [isPending, 늦게처리] = useTransition();
+  let state = useDeferredValue(name);
+
   return (
-    <TestModal />
+    <>
+      <input
+        onChange={(e) => {
+          늦게처리(() => {
+            setName(e.target.name);
+          });
+        }}
+      />
+      {isPending
+        ? "로딩중"
+        : a.map(() => {
+            return <div>{state}</div>;
+          })}
+    </>
   );
 }
 
