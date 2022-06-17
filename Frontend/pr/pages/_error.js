81ef9233320fe54d70fe300270446682cafc637c
kimkh05 <nestjs01@naver.com>
2022-06-17 20:27:30 +0900

    주석 추가

--- Frontend/pr/pages/_error.js
+++ Frontend/pr/pages/_error.js
@@ -9,7 +9,7 @@ function Error({ statusCode }) {
 }
 
 Error.getInitalProps = ({ res, err }) => {
-  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404; // status code 설정해주기 404 or 500과 같은 것을 말한다. 
   return { statusCode };
 };
 
