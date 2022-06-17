fe17066df1b6ed1a6a97e896e5a89a4794317bb2
kimkh05 <nestjs01@naver.com>
2022-06-17 20:21:52 +0900

    아직 만들다 만 코드

---
+++ Algorithm/Baekjoon/11651/c.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+typedef struct{
+    int x, y
+}
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    Point p[n];
+    for(int i = 0; i < n; i++){
+        scanf("%d %d", &)
+    }
+}
