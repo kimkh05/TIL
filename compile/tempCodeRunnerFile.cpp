db7dd91e3e7696c646809fed0121f43da464e8ce
kimkh05 <nestjs01@naver.com>
2022-07-11 14:11:42 +0900

    누적합 해결

--- compile/tempCodeRunnerFile.cpp
+++ compile/tempCodeRunnerFile.cpp
@@ -1,6 +1,2 @@
-
-    min[i] = str[i] - '0';
-    arr1[i] = str[i] - '0';
-  }
-  sort(min, min +n);
-  for(int i = 0; i < n;i++){
+#include<stdio.h>
+int main(){ int a, b; scanf("%d %d", &a, &b); printf(a == b ? "OLDBIE!" : a > b ? "NEWBIE!" : "TLE!"); }
