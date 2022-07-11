2cb52afc62b23ef2f99d43edd1f0de272703fbd3
kimkh05 <nestjs01@naver.com>
2022-07-11 15:19:04 +0900

    이다솜 문제 해결

--- compile/c.cpp
+++ compile/c.cpp
@@ -1,19 +1,51 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
-#define MAX 100001
+#define P1(x)(x+1)
+#define M1(x)(x-1)
+
+typedef struct{
+    int number;
+    char name[21];
+}Pocketmon;
+
+int compare(const void* a, const void* b){
+    Pocketmon* c = (Pocketmon *)a;
+    Pocketmon* d = (Pocketmon *)b;
+    return strcmp(c->name, d->name) > 0 ? 1 : -1;
+}
 
 int main(){
-    int n, m, arr[MAX] = {0, }, a, b;
-    long long sum[MAX] = {0, }, answer = 0;
+    int n, m, left, right, mid;
+    char str[21];
     scanf("%d %d", &n, &m);
-    for(int i = 1; i <= n; i++){
-        scanf("%d", &arr[i]);
-        sum[i] = sum[i-1] + arr[i];
+    Pocketmon *pocketmon = (Pocketmon *)malloc(sizeof(Pocketmon) * n); // 입력 배열
+    Pocketmon *sorted = (Pocketmon *)malloc(sizeof(Pocketmon) * n); // 정렬 배열
+    for(int i = 0; i < n; i++){
+        scanf("%s", pocketmon[i].name);
+        pocketmon[i].number = P1(i);
+        sorted[i].number = P1(i);
+        sorted[i] = pocketmon[i]; 
     }
-    for(int i = 0; i < m; i++){
-        scanf("%d %d", &a, &b);
-        answer = sum[b] - sum[a - 1];
-        printf("%d\n", answer);
+    qsort(sorted, n, sizeof(sorted[0]), compare);
+    for(int i = 0 ; i < m; i++){
+        scanf(" %s", str);
+        if(str[0] >= '0' && str[0] <= '9') {
+            printf("%s\n", pocketmon[M1(atoi(str))].name);
+        } else {
+            left = 0, right = n - 1;
+            while(left <= right){
+                mid = (left + right) / 2;
+                if(strcmp(sorted[mid].name, str) == 0){
+                    printf("%d\n", sorted[mid].number);
+                    break;
+                } else if(strcmp(sorted[mid].name, str) > 0) right = M1(mid);
+                else left = P1(mid);
+            }
+        }
     }
+    free(pocketmon);
+    free(sorted);
     return 0;
 }
