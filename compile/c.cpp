25a16cd7308b64cbb563ff278aac249663c5f369
kimkh05 <nestjs01@naver.com>
2022-07-13 23:47:52 +0900

    알고리즘 문제 핵려

--- compile/c.cpp
+++ compile/c.cpp
@@ -1,14 +1,27 @@
 #include<stdio.h>
+#include<string.h>
 
-#define true 1
-#define false 0
+#define MAXLEN 51
 
 int main(){
-    int a, b, c, d, sum = 0;
-    for(int i = 0; i < 2; i++){
-        scanf("%d %d %d %d", &a, &b, &c, &d);
-        if(a + b+ c + d > sum) sum = a+b+c+d;
+    int sum = 0, temp = 0, arr[MAXLEN / 2], tmp = 0, answer;
+    char str[MAXLEN];
+    scanf("%s", str);
+    for(int i = 0 ; i <= strlen(str); i++){
+        if(str[i] == '-' || i == strlen(str)){
+            sum += temp;
+            arr[tmp++] = sum;
+            temp = sum = 0;
+        } else if(str[i] == '+'){
+            sum += temp;
+            temp = 0;
+        } else{
+            temp *= 10;
+            temp += str[i] - '0';
+        }
     }
-    printf("%d\n", sum);
+    answer = arr[0];
+    for(int i = 1; i < tmp; i++) answer -= arr[i];
+    printf("%d\n", answer); 
     return 0;
 }
